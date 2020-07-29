af4c54343e1ba42fc194c7a500037a9ea437359d
Gaiday_u <ura.kiev@gmail.com>
2020-07-29 15:39:07 +0300

    Updates

---
+++ precache-manifest.5abd0fb041b70df2abed2d87bcf222a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5976285e1dffec8efb41b961daabc0dc",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "51d5d0888b243a78c999",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.fc5687c4.chunk.css"
+  },
+  {
+    "revision": "ba4d645d983f01068857",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.861b287d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.861b287d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d5d0888b243a78c999",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.d2092657.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
